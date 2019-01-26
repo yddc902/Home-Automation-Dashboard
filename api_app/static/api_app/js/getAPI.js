8e073f137e421aa39d2b07cbeeeb800e6d04bd6d
Ridge <enfaduraa@gmail.com>
2019-01-25 20:35:13 -0500

    Reduce JS calls to a single function

--- api_app/static/api_app/js/getAPI.js
+++ api_app/static/api_app/js/getAPI.js
@@ -1,66 +1,28 @@
-/*$(function getTemp() {
-  $.ajax({
-    type: 'GET',
-    url: '/api/room1',
-    //url: {% url 'last_temp' %}
-    //url: '/api/TempModel/?format=json',
-    dataType: 'json',
-    success:function(data){
-      console.log(data);
-      $("#room1").append(data)
-    }
-  })
-})*/
+var refreshInterval = 15; //Refresh data interval
 
-setInterval(function() {
-  $(function getTemp() {
-    $.getJSON('/api/temperatures/kitchen', {
-      format: "json"
-    }).done(function(data){
-      console.log(data);
-      $.each(data, function(i, item) {
-        console.log(item.fields.temperature_f);
-        $("#kitchen").empty().append(item.fields.temperature_f + '&#8457;');
-      })
-    })
-    .error(function() {
-      $("#kitchen").append("Err")
+//Retrieve temp data from API
+function getTemp(room) {
+  $.getJSON('/api/temperatures/' + room, {
+    format: "json"
+  }).done(function(data){
+    console.log(data);
+    $.each(data, function(i, item) {
+      console.log(item.fields.temperature_f);
+      $("#" + room).empty().append(item.fields.temperature_f + '&#8457;');      // .empty() removes old data before appending new data
     })
   })
+}
 
-  $(function getTemp() {
-    $.getJSON('/api/temperatures/livingroom', {
-      format: "json"
-    }).done(function(data){
-      console.log(data);
-      $.each(data, function(i, item) {
-        console.log(item.fields.temperature_f);
-        $("#livingroom").empty().append(item.fields.temperature_f + '&#8457;');
-      })
-    })
-  })
+//Retrieve data as soon as the page loads
+getTemp("kitchen");
+getTemp("mancave");
+getTemp("livingroom");
+getTemp("bedroom");
 
-  $(function getTemp() {
-    $.getJSON('/api/temperatures/mancave', {
-      format: "json"
-    }).done(function(data){
-      console.log(data);
-      $.each(data, function(i, item) {
-        console.log(item.fields.temperature_f);
-        $("#mancave").empty().append(item.fields.temperature_f + '&#8457;');
-      })
-    })
-  })
-
-  $(function getTemp() {
-    $.getJSON('/api/temperatures/bedroom', {
-      format: "json"
-    }).done(function(data){
-      console.log(data);
-      $.each(data, function(i, item) {
-        console.log(item.fields.temperature_f);
-        $("#mancave").empty().append(item.fields.temperature_f + '&#8457;');
-      })
-    })
-  })
-}, 15000);
+//Refresh data every 15 seconds
+setInterval( function() {
+  getTemp("kitchen");
+  getTemp("mancave");
+  getTemp("livingroom");
+  getTemp("bedroom");
+}, refreshInterval * 1000);
