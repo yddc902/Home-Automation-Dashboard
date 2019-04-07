e3a136a6c145f1209ceba7950a748c31cc6ca79e
Ridge <enfaduraa@gmail.com>
2019-04-07 10:19:14 -0400

    Fixed charts URLs

--- chart_app/urls.py
+++ chart_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('charts/', views.charts, name='jscharts'),
-    path('charts/temperature', views.all_temp_charts, name="all_temp"),
-    path('charts/temperature/<str:room>', views.temp_charts, name="temp_charts"),
+    path('', views.charts, name='jscharts'),
+    path('temperature/', views.all_temp_charts, name="all_temp"),
+    path('temperature/<str:room>', views.temp_charts, name="temp_charts"),
 ]
